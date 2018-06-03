57db44be7debf40d54b6a257a696936cf31e4689
= <abirenbaum721@gmail.com>
2018-06-03 18:24:12 -0500

    Johannah

--- ui.R
+++ ui.R
@@ -8,7 +8,7 @@ ui <- bootstrapPage(
   leafletOutput("mymap", width = "100%", height = "100%"),
   absolutePanel(top = 100, left = 20, width = 300,
                 
-                sliderInput("rating","Rating >= ",min = 0, max = 5, step = 0.5, value = 3),
+                sliderInput("rating","Johannah is the most beautiful person in the whole wide world!!",min = 0, max = 5, step = 0.5, value = 3),
                 sliderInput("n_reviews","Minimum Reviews", min =0, max = 200, step = 10, value = 10),
                 selectizeInput("category","Filter by Categories",choices = all_categories,multiple = T),
                 style = "opacity: 0.9; z-index: 1000;"
